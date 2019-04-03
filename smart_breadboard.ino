173e173da499e346203449f786bab65e56d90443
Caleb Davenport <caleb@calebdavenport.com>
2019-04-03 10:44:35 -0400

    Remove more unnecessary code

--- smart_breadboard.ino
+++ smart_breadboard.ino
@@ -61,19 +61,10 @@ ft_char8_t FT_DispCSpread = 1;
 ft_char8_t FT_DispDither = 1;
 
 #define ON          1
-#define OFF         0						 
+#define OFF         0
 #define Font        27					// Font Size
 #define MAX_LINES   4					// Max Lines allows to Display
 
-#define SPECIAL_FUN     251
-#define BACK_SPACE	251				// Back space
-#define CAPS_LOCK	252				// Caps Lock
-#define NUMBER_LOCK	253				// Number Lock
-#define BACK		254				// Exit 
-
-#define LINE_STARTPOS	FT_DispWidth/50			// Start of Line
-#define LINE_ENDPOS	FT_DispWidth			// max length of the line
-		
 /* Global used for buffer optimization */
 Ft_Gpu_Hal_Context_t host,*phost;
 
@@ -87,7 +78,7 @@ ft_uint8_t  Ft_CmdBuffer[FT_CMD_FIFO_SIZE];
 #endif
 /* Boot up for FT800 followed by graphics primitive sample cases */
 /* Initial boot up DL - make the back ground green color */
-const ft_uint8_t FT_DLCODE_BOOTUP[12] = 
+const ft_uint8_t FT_DLCODE_BOOTUP[12] =
 {
     0,0,0,2,//GPU instruction CLEAR_COLOR_RGB
     7,0,0,38, //GPU instruction CLEAR
@@ -102,215 +93,39 @@ ft_void_t Ft_App_WrCoCmd_Buffer(Ft_Gpu_Hal_Context_t *phost,ft_uint32_t cmd)
    pBuffcmd =(ft_uint32_t*)&Ft_CmdBuffer[Ft_CmdBuffer_Index];
    *pBuffcmd = cmd;
 #endif
-#ifdef ARDUINO_PLATFORM
    Ft_Gpu_Hal_WrCmd32(phost,cmd);
-#endif
-#ifdef FT900_PLATFORM
-   Ft_Gpu_Hal_WrCmd32(phost,cmd);
-#endif
    /* Increment the command index */
-   Ft_CmdBuffer_Index += FT_CMD_SIZE;  
-}
-
-ft_void_t Ft_App_WrDlCmd_Buffer(Ft_Gpu_Hal_Context_t *phost,ft_uint32_t cmd)
-{
-#ifdef BUFFER_OPTIMIZATION  
-   /* Copy the command instruction into buffer */
-   ft_uint32_t *pBuffcmd;
-   pBuffcmd =(ft_uint32_t*)&Ft_DlBuffer[Ft_DlBuffer_Index];
-   *pBuffcmd = cmd;
-#endif
-
-#ifdef ARDUINO_PLATFORM
-   Ft_Gpu_Hal_Wr32(phost,(RAM_DL+Ft_DlBuffer_Index),cmd);
-#endif
-#ifdef FT900_PLATFORM
-   Ft_Gpu_Hal_Wr32(phost,(RAM_DL+Ft_DlBuffer_Index),cmd);
-#endif
-   /* Increment the command index */
-   Ft_DlBuffer_Index += FT_CMD_SIZE;  
-}
-
-ft_void_t Ft_App_WrCoStr_Buffer(Ft_Gpu_Hal_Context_t *phost,const ft_char8_t *s)
-{
-#ifdef  BUFFER_OPTIMIZATION  
-  ft_uint16_t length = 0;
-  length = strlen(s) + 1;//last for the null termination
-  
-  strcpy(&Ft_CmdBuffer[Ft_CmdBuffer_Index],s);  
-
-  /* increment the length and align it by 4 bytes */
-  Ft_CmdBuffer_Index += ((length + 3) & ~3);  
-#endif  
-}
-
-ft_void_t Ft_App_Flush_DL_Buffer(Ft_Gpu_Hal_Context_t *phost)
-{
-#ifdef  BUFFER_OPTIMIZATION    
-   if (Ft_DlBuffer_Index > 0)
-     Ft_Gpu_Hal_WrMem(phost,RAM_DL,Ft_DlBuffer,Ft_DlBuffer_Index);
-#endif     
-   Ft_DlBuffer_Index = 0;
-   
+   Ft_CmdBuffer_Index += FT_CMD_SIZE;
 }
 
 ft_void_t Ft_App_Flush_Co_Buffer(Ft_Gpu_Hal_Context_t *phost)
 {
-#ifdef  BUFFER_OPTIMIZATION    
+#ifdef  BUFFER_OPTIMIZATION
    if (Ft_CmdBuffer_Index > 0)
      Ft_Gpu_Hal_WrCmdBuf(phost,Ft_CmdBuffer,Ft_CmdBuffer_Index);
-#endif     
+#endif
    Ft_CmdBuffer_Index = 0;
 }
 
 
-/* API to give fadeout effect by changing the display PWM from 100 till 0 */
-ft_void_t SAMAPP_fadeout()
-{
-   ft_int32_t i;
-	
-	for (i = 100; i >= 0; i -= 3) 
-	{
-		Ft_Gpu_Hal_Wr8(phost,REG_PWM_DUTY,i);
-
-		Ft_Gpu_Hal_Sleep(2);//sleep for 2 ms
-	}
-}
-
-/* API to perform display fadein effect by changing the display PWM from 0 till 100 and finally 128 */
-ft_void_t SAMAPP_fadein()
-{
-	ft_int32_t i;
-	
-	for (i = 0; i <=100 ; i += 3) 
-	{
-		Ft_Gpu_Hal_Wr8(phost,REG_PWM_DUTY,i);
-		Ft_Gpu_Hal_Sleep(2);//sleep for 2 ms
-	}
-	/* Finally make the PWM 100% */
-	i = 128;
-	Ft_Gpu_Hal_Wr8(phost,REG_PWM_DUTY,i);
-}
-#ifdef SAMAPP_ENABLE_APIS_SET0
-
-/* Optimized implementation of sin and cos table - precision is 16 bit */
-FT_PROGMEM ft_prog_uint16_t sintab[] = {
-0, 402, 804, 1206, 1607, 2009, 2410, 2811, 3211, 3611, 4011, 4409, 4807, 5205, 5601, 5997, 6392, 
-6786, 7179, 7571, 7961, 8351, 8739, 9126, 9511, 9895, 10278, 10659, 11038, 11416, 11792, 12166, 12539,
-12909, 13278, 13645, 14009, 14372, 14732, 15090, 15446, 15799, 16150, 16499, 16845, 17189, 17530, 17868,
-18204, 18537, 18867, 19194, 19519, 19840, 20159, 20474, 20787, 21096, 21402, 21705, 22004, 22301, 22594, 
-22883, 23169, 23452, 23731, 24006, 24278, 24546, 24811, 25072, 25329, 25582, 25831, 26077, 26318, 26556, 26789, 
-27019, 27244, 27466, 27683, 27896, 28105, 28309, 28510, 28706, 28897, 29085, 29268, 29446, 29621, 29790, 29955, 
-30116, 30272, 30424, 30571, 30713, 30851, 30984, 31113, 31236, 31356, 31470, 31580, 31684, 31785, 31880, 31970, 
-32056, 32137, 32213, 32284, 32350, 32412, 32468, 32520, 32567, 32609, 32646, 32678, 32705, 32727, 32744, 32757, 
-32764, 32767, 32764};
-
-ft_int16_t SAMAPP_qsin(ft_uint16_t a)
-{
-  ft_uint8_t f;
-  ft_int16_t s0,s1;
-
-  if (a & 32768)
-    return -SAMAPP_qsin(a & 32767);
-  if (a & 16384)
-      a = 32768 - a;
-  f = a & 127;
-  s0 = ft_pgm_read_word(sintab + (a >> 7));
-  s1 = ft_pgm_read_word(sintab + (a >> 7) + 1);
-  return (s0 + ((ft_int32_t)f * (s1 - s0) >> 7));
-}
-
-/* cos funtion */
-ft_int16_t SAMAPP_qcos(ft_uint16_t a)
-{
-  return (SAMAPP_qsin(a + 16384));
-}
-#endif
-
-
-/* API to check the status of previous DLSWAP and perform DLSWAP of new DL */
-/* Check for the status of previous DLSWAP and if still not done wait for few ms and check again */
-ft_void_t SAMAPP_GPU_DLSwap(ft_uint8_t DL_Swap_Type)
-{
-	ft_uint8_t Swap_Type = DLSWAP_FRAME,Swap_Done = DLSWAP_FRAME;
-
-	if(DL_Swap_Type == DLSWAP_LINE)
-	{
-		Swap_Type = DLSWAP_LINE;
-	}
-
-	/* Perform a new DL swap */
-	Ft_Gpu_Hal_Wr8(phost,REG_DLSWAP,Swap_Type);
-
-	/* Wait till the swap is done */
-	while(Swap_Done)
-	{
-		Swap_Done = Ft_Gpu_Hal_Rd8(phost,REG_DLSWAP);
-
-		if(DLSWAP_DONE != Swap_Done)
-		{
-			Ft_Gpu_Hal_Sleep(10);//wait for 10ms
-		}
-	}	
-}
-
-/* deflated icons*/
-static ft_uint8_t home_star_icon[] = {0x78,0x9C,0xE5,0x94,0xBF,0x4E,0xC2,0x40,0x1C,0xC7,0x7F,0x2D,0x04,0x8B,0x20,0x45,0x76,0x14,0x67,0xA3,0xF1,0x0D,0x64,0x75,0xD2,0xD5,0x09,0x27,0x17,0x13,0xE1,0x0D,0xE4,0x0D,0x78,0x04,0x98,0x5D,0x30,0x26,0x0E,0x4A,0xA2,0x3E,0x82,0x0E,0x8E,0x82,0xC1,0x38,0x62,0x51,0x0C,0x0A,0x42,0x7F,0xDE,0xB5,0x77,0xB4,0x77,0x17,0x28,0x21,0x26,0x46,0xFD,0x26,0xCD,0xE5,0xD3,0x7C,0xFB,0xBB,0xFB,0xFD,0xB9,0x02,0xCC,0xA4,0xE8,0x99,0x80,0x61,0xC4,0x8A,0x9F,0xCB,0x6F,0x31,0x3B,0xE3,0x61,0x7A,0x98,0x84,0x7C,0x37,0xF6,0xFC,0xC8,0xDD,0x45,0x00,0xDD,0xBA,0xC4,0x77,0xE6,0xEE,0x40,0xEC,0x0E,0xE6,0x91,0xF1,0xD2,0x00,0x42,0x34,0x5E,0xCE,0xE5,0x08,0x16,0xA0,0x84,0x68,0x67,0xB4,0x86,0xC3,0xD5,0x26,0x2C,0x20,0x51,0x17,0xA2,0xB8,0x03,0xB0,0xFE,0x49,0xDD,0x54,0x15,0xD8,0xEE,0x73,0x37,0x95,0x9D,0xD4,0x1A,0xB7,0xA5,0x26,0xC4,0x91,0xA9,0x0B,0x06,0xEE,0x72,0xB7,0xFB,0xC5,0x16,0x80,0xE9,0xF1,0x07,0x8D,0x3F,0x15,0x5F,0x1C,0x0B,0xFC,0x0A,0x90,0xF0,0xF3,0x09,0xA9,0x90,0xC4,0xC6,0x37,0xB0,0x93,0xBF,0xE1,0x71,0xDB,0xA9,0xD7,0x41,0xAD,0x46,0xEA,0x19,0xA9,0xD5,0xCE,0x93,0xB3,0x35,0x73,0x0A,0x69,0x59,0x91,0xC3,0x0F,0x22,0x1B,0x1D,0x91,0x13,0x3D,0x91,0x73,0x43,0xF1,0x6C,0x55,0xDA,0x3A,0x4F,0xBA,0x25,0xCE,0x4F,0x04,0xF1,0xC5,0xCF,0x71,0xDA,0x3C,0xD7,0xB9,0xB2,0x48,0xB4,0x89,0x38,0x20,0x4B,0x2A,0x95,0x0C,0xD5,0xEF,0x5B,0xAD,0x96,0x45,0x8A,0x41,0x96,0x7A,0x1F,0x60,0x0D,0x7D,0x22,0x75,0x82,0x2B,0x0F,0xFB,0xCE,0x51,0x3D,0x2E,0x3A,0x21,0xF3,0x1C,0xD9,0x38,0x86,0x2C,0xC6,0x05,0xB6,0x7B,0x9A,0x8F,0x0F,0x97,0x1B,0x72,0x6F,0x1C,0xEB,0xAE,0xFF,0xDA,0x97,0x0D,0xBA,0x43,0x32,0xCA,0x66,0x34,0x3D,0x54,0xCB,0x24,0x9B,0x43,0xF2,0x70,0x3E,0x42,0xBB,0xA0,0x95,0x11,0x37,0x46,0xE1,0x4F,0x49,0xC5,0x1B,0xFC,0x3C,0x3A,0x3E,0xD1,0x65,0x0E,0x6F,0x58,0xF8,0x9E,0x5B,0xDB,0x55,0xB6,0x41,0x34,0xCB,0xBE,0xDB,0x87,0x5F,0xA9,0xD1,0x85,0x6B,0xB3,0x17,0x9C,0x61,0x0C,0x9B,0xA2,0x5D,0x61,0x10,0xED,0x2A,0x9B,0xA2,0x5D,0x61,0x10,0xED,0x2A,0x9B,0xA2,0x5D,0x61,0x10,0xED,0x2A,0x9B,0xED,0xC9,0xFC,0xDF,0x14,0x54,0x8F,0x80,0x7A,0x06,0xF5,0x23,0xA0,0x9F,0x41,0xF3,0x10,0x30,0x4F,0x41,0xF3,0x18,0x30,0xCF,0xCA,0xFC,0xFF,0x35,0xC9,0x79,0xC9,0x89,0xFA,0x33,0xD7,0x1D,0xF6,0x5E,0x84,0x5C,0x56,0x6E,0xA7,0xDA,0x1E,0xF9,0xFA,0xAB,0xF5,0x97,0xFF,0x2F,0xED,0x89,0x7E,0x29,0x9E,0xB4,0x9F,0x74,0x1E,0x69,0xDA,0xA4,0x9F,0x81,0x94,0xEF,0x4F,0xF6,0xF9,0x0B,0xF4,0x65,0x51,0x08};
-
-
-struct 
+struct
 {
 	ft_uint8_t Key_Detect :1;
 	ft_uint8_t Caps :1;
 	ft_uint8_t Numeric : 1;
-	ft_uint8_t Exit : 1;	
+	ft_uint8_t Exit : 1;
 }Flag;
-
-// Buffer for the notepads
-struct Notepad_buffer
-{
-	ft_char8_t *temp;
-	ft_char8_t  notepad[MAX_LINES][80];
-}Buffer;
-
-
-// Api to play Key sound 
-ft_void_t Play_Sound(ft_uint8_t sound,ft_uint8_t volume)
-{
-  Ft_Gpu_Hal_Wr8(phost,REG_VOL_SOUND,volume);
-  Ft_Gpu_Hal_Wr8(phost,REG_SOUND,sound);
-  Ft_Gpu_Hal_Wr8(phost,REG_PLAY,1);
-}	
 
 static byte istouch()
 {
   return !(Ft_Gpu_Hal_Rd16(phost,REG_TOUCH_RAW_XY) & 0x8000);
 }
 static ft_uint8_t sk=0;
-/********API to return the assigned TAG value when penup,for the primitives/widgets******/
-ft_uint8_t Read_keys()
-{
-  static ft_uint8_t Read_tag=0,temp_tag=0,ret_tag=0;	
-  Read_tag = Ft_Gpu_Hal_Rd8(phost,REG_TOUCH_TAG);
-  ret_tag = NULL;
-  if(Read_tag!=NULL && temp_tag!=Read_tag)
-  {
-     temp_tag = Read_tag;											// Load the Read tag to temp variable	
-     sk = Read_tag;			
-  } 
-  if(Read_tag==NULL)
-  {
-     ret_tag =  temp_tag;
-     temp_tag = 0;
-     sk = 0;
-  }
-  return ret_tag;
-}
 
 
 ft_uint8_t Read_Keypad()
 {
-  static ft_uint8_t Read_tag=0,temp_tag=0,ret_tag=0,touch_detect=1;	
+  static ft_uint8_t Read_tag=0,temp_tag=0,ret_tag=0,touch_detect=1;
   Read_tag = Ft_Gpu_Hal_Rd8(phost,REG_TOUCH_TAG);
   ret_tag = NULL;
   if(istouch()==0)  touch_detect = 0;
@@ -318,24 +133,14 @@ ft_uint8_t Read_Keypad()
   {
     if(temp_tag!=Read_tag && touch_detect==0)
     {
-      temp_tag = Read_tag;											// Load the Read tag to temp variable	
-      Play_Sound(0x51,100);
+      temp_tag = Read_tag;											// Load the Read tag to temp variable
       touch_detect = 1;
     }
-/*    if(touch_detect==1)
-    {
-      if(temp_tag != Read_tag)											
-      {
-        temp_tag = 0;
-        Flag.Key_Detect = 0;
-        Read_tag = 0;
-      }
-    }*/
   }
   else
   {
     if(temp_tag!=0)
-    {	
+    {
       Flag.Key_Detect = 1;
       Read_tag = temp_tag;
     }
@@ -351,7 +156,7 @@ void Notepad(void)
   ft_uint8_t Line=0;
   ft_uint16_t Disp_pos = 0,But_opt;
   ft_uint8_t Read_sfk=0,	tval;
-  ft_uint16_t noofchars=0,line2disp =0,nextline = 0;	
+  ft_uint16_t noofchars=0,line2disp =0,nextline = 0;
   ft_uint8_t   font = 27;
 
   const ft_uint16_t NEXT_SFK = 201;
@@ -401,16 +206,16 @@ void Notepad(void)
   Flag.Exit = 0;
   do
   {
-    Read_sfk = Read_Keypad();                // read the keys   
-     
-		
+    Read_sfk = Read_Keypad();                // read the keys
+
+
 // Start the new Display list
-  Ft_Gpu_CoCmd_Dlstart(phost); 
-  Ft_App_WrCoCmd_Buffer(phost,CLEAR_COLOR_RGB(100,100,100));        
-  Ft_App_WrCoCmd_Buffer(phost,CLEAR(1,1,1));	
+  Ft_Gpu_CoCmd_Dlstart(phost);
+  Ft_App_WrCoCmd_Buffer(phost,CLEAR_COLOR_RGB(100,100,100));
+  Ft_App_WrCoCmd_Buffer(phost,CLEAR(1,1,1));
   Ft_App_WrCoCmd_Buffer(phost,COLOR_RGB(255,255,255));
   Ft_App_WrCoCmd_Buffer(phost,TAG_MASK(1));            // enable tagbuffer updation
-  Ft_Gpu_CoCmd_FgColor(phost,0x993366);  
+  Ft_Gpu_CoCmd_FgColor(phost,0x663300);
   Ft_Gpu_CoCmd_BgColor(phost,0x662244);
 
   if (Read_sfk == NEXT_SFK && current_state + 1 < NUM_STATES) {
@@ -446,7 +251,7 @@ void Notepad(void)
   Ft_Gpu_CoCmd_Swap(phost);
   Ft_App_Flush_Co_Buffer(phost);
   Ft_Gpu_Hal_WaitCmdfifo_empty(phost);
- }while(1);		
+ }while(1);
 
 }
 /***********************API used to SET the ICON******************************************/
@@ -457,7 +262,7 @@ ft_void_t Info()
   ft_uint16_t dloffset = 0,z;
   Ft_CmdBuffer_Index = 0;
   // Touch Screen Calibration
-  Ft_Gpu_CoCmd_Dlstart(phost); 
+  Ft_Gpu_CoCmd_Dlstart(phost);
   Ft_App_WrCoCmd_Buffer(phost,CLEAR(1,1,1));
   Ft_App_WrCoCmd_Buffer(phost,COLOR_RGB(255,255,255));
   Ft_Gpu_CoCmd_Text(phost,FT_DispWidth/2,FT_DispHeight/2 - 15,26,OPT_CENTERX|OPT_CENTERY,"Touch Screen Calibration");
@@ -496,64 +301,7 @@ ft_void_t Ft_BootupConfig()
 			printf("VC1 register ID after wake up %x\n",chipid);
 	#endif
 	}
-	/* Configuration of LCD display */
-#ifdef DISPLAY_RESOLUTION_QVGA
-	/* Values specific to QVGA LCD display */
-	FT_DispWidth = 320;
-	FT_DispHeight = 240;
-	FT_DispHCycle =  408;
-	FT_DispHOffset = 70;
-	FT_DispHSync0 = 0;
-	FT_DispHSync1 = 10;
-	FT_DispVCycle = 263;
-	FT_DispVOffset = 13;
-	FT_DispVSync0 = 0;
-	FT_DispVSync1 = 2;
-	FT_DispPCLK = 8;
-	FT_DispSwizzle = 2;
-	FT_DispPCLKPol = 0;
-	FT_DispCSpread = 1;
-	FT_DispDither = 1;
 
-#endif
-#ifdef DISPLAY_RESOLUTION_WVGA
-	/* Values specific to QVGA LCD display */
-	FT_DispWidth = 800;
-	FT_DispHeight = 480;
-	FT_DispHCycle =  928;
-	FT_DispHOffset = 88;
-	FT_DispHSync0 = 0;
-	FT_DispHSync1 = 48;
-	FT_DispVCycle = 525;
-	FT_DispVOffset = 32;
-	FT_DispVSync0 = 0;
-	FT_DispVSync1 = 3;
-	FT_DispPCLK = 2;
-	FT_DispSwizzle = 0;
-	FT_DispPCLKPol = 1;
-	FT_DispCSpread = 0;
-	FT_DispDither = 1;
-#endif
-#ifdef DISPLAY_RESOLUTION_HVGA_PORTRAIT
-	/* Values specific to HVGA LCD display */
-
-	FT_DispWidth = 320;
-	FT_DispHeight = 480;
-	FT_DispHCycle =  400;
-	FT_DispHOffset = 40;
-	FT_DispHSync0 = 0;
-	FT_DispHSync1 = 10;
-	FT_DispVCycle = 500;
-	FT_DispVOffset = 10;
-	FT_DispVSync0 = 0;
-	FT_DispVSync1 = 5;
-	FT_DispPCLK = 4;
-	FT_DispSwizzle = 2;
-	FT_DispPCLKPol = 1;
-	FT_DispCSpread = 1;
-	FT_DispDither = 1;
-
-#endif
 	Ft_Gpu_Hal_Wr16(phost, REG_HCYCLE, FT_DispHCycle);
 	Ft_Gpu_Hal_Wr16(phost, REG_HOFFSET, FT_DispHOffset);
 	Ft_Gpu_Hal_Wr16(phost, REG_HSYNC0, FT_DispHSync0);
@@ -585,32 +333,7 @@ ft_void_t Ft_BootupConfig()
     Ft_Gpu_Hal_Wr8(phost, REG_PCLK,FT_DispPCLK);//after this display is visible on the LCD
 
 
-#ifdef ENABLE_ILI9488_HVGA_PORTRAIT
-	/* to cross check reset pin */
-	Ft_Gpu_Hal_Wr8(phost, REG_GPIO,0xff);
-	ft_delay(120);
-	Ft_Gpu_Hal_Wr8(phost, REG_GPIO,0x7f);
-	ft_delay(120);
-	Ft_Gpu_Hal_Wr8(phost, REG_GPIO,0xff);
 
-//	ILI9488_Bootup();
-	
-	/* Reconfigure the SPI to communicate with FT8xx */
-//spi
-	// Initialize SPIM HW
-	sys_enable(sys_device_spi_master);
-	gpio_function(27, pad_spim_sck); /* GPIO27 to SPIM_CLK */
-	gpio_function(28, pad_spim_ss0); /* GPIO28 as CS */
-	gpio_function(29, pad_spim_mosi); /* GPIO29 to SPIM_MOSI */
-	gpio_function(30, pad_spim_miso); /* GPIO30 to SPIM_MISO */
-
-	gpio_write(28, 1);
-	spi_init(SPIM, spi_dir_master, spi_mode_0, 4);//SPISysInit(SPIM);	
-#ifdef FT900_PLATFORM
-	printf("aftrer ILI9488 bootup \n");
-#endif
-
-#endif
 
 	/* make the spi to quad mode - addition 2 bytes for silicon */
 #ifdef FT_81X_ENABLE
@@ -632,41 +355,22 @@ phost->ft_cmd_fifo_wp = Ft_Gpu_Hal_Rd16(phost,REG_CMD_WRITE);
 /* Main entry point */
 ft_void_t setup() {
 
-	 ft_uint8_t chipid;
+	ft_uint8_t chipid;
 	Ft_Gpu_HalInit_t halinit;
-	
 	halinit.TotalChannelNum = 1;
-
-
   Ft_Gpu_Hal_Init(&halinit);
   host.hal_config.channel_no = 0;
   host.hal_config.pdn_pin_no = FT800_PD_N;
   host.hal_config.spi_cs_pin_no = FT800_SEL_PIN;
-  
   host.hal_config.spi_clockrate_khz = 4000; //in KHz
-  
-  Ft_Gpu_Hal_Open(&host);
 
+  Ft_Gpu_Hal_Open(&host);
   phost = &host;
 
-    Ft_BootupConfig();
-
-    /*It is optional to clear the screen here*/	
-    Ft_Gpu_Hal_WrMem(phost, RAM_DL,(ft_uint8_t *)FT_DLCODE_BOOTUP,sizeof(FT_DLCODE_BOOTUP));
-    Ft_Gpu_Hal_Wr8(phost, REG_DLSWAP,DLSWAP_FRAME);
-    
-    //Ft_Gpu_Hal_Sleep(1000);//Show the booting up screen.
-
-
-
-  //home_setup();
-  Info();
+  Ft_BootupConfig();
+  Info(); //Set screen calibration
   Ft_Gpu_Hal_Sleep(100);
-  Notepad();
-
-#ifdef MSVC_PLATFORM
-  return 0;
-#endif
+  Notepad(); // Main loop
 }
 
 void loop()
